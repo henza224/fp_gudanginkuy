cd4d9c855e17535d8ce683b9a52b502bae9aed45
ryobroza <ryobroza@gmail.com>
2024-07-20 14:32:53 +0700

    [UPDATE] penambahan method logout pada AuthController dan ErrorHandler

---
+++ src/main/java/finalproject/gudanginkuy/d_controller/ErrorHandlerController.java
@@ -0,0 +1,29 @@
+package finalproject.gudanginkuy.d_controller;
+
+import finalproject.gudanginkuy.utils.response.Res;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+
+@RestControllerAdvice
+public class ErrorHandlerController {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgument(MethodArgumentNotValidException e) {
+        String message = "Validation Error";
+        if (e.getBindingResult().hasErrors()) {
+            message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        }
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        return Res.renderJson(null, message, status);
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<?> handleResponseStatusException(ResponseStatusException e) {
+        return Res.renderJson(null, e.getReason(), HttpStatus.BAD_REQUEST);
+    }
+}
